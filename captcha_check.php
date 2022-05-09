8949f5b88b2594a4c7230646508877255020e2b2
Torio <jpstorio@mymail.mapua.edu.ph>
2022-05-09 13:40:57 +0800

    first commit

---
+++ captcha_check.php
@@ -0,0 +1,28 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => 'response='.$_REQUEST['captcha'].'&secret=6LcyMdMfAAAAAMGlNjOt_SeQyMjDEr4M8sOCGmns',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/x-www-form-urlencoded'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+$array_res = json_decode($response, true);
+if($array_res["success"]=="true"){
+echo "Login Successful";
+} else {
+echo "Login Failed";
+}
